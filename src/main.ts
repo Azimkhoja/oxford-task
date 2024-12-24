8b3430181610129efd78a4a2fc2a166fce0d6bd6
Azimkhoja <117832130+Azimkhoja@users.noreply.github.com>
2024-12-24 18:18:26 +0500

    env config set

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import * as dotenv from 'dotenv'
+
+dotenv.config() // dotenv varianti environment ozgaruvchilarni ishlatish uchun 
+//const configService = app.get(ConfigService) // configservice bu ham env variablelarni ishlatish uchun  
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  
+  const port = process.env.PORT
+  console.log(process.env.DB_DATABASE)
+
+  await app.listen(port ?? 3000, () => {
+    console.log(`server started on port ${port}`)}
+    );
 }
 bootstrap();
