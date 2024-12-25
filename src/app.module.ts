0ea6ca5e7091a9de685a069881ad4d15bc33a96c
azimkhoja <ahmadkhonovazimkhoja@gmail.com>
2024-12-25 07:46:27 +0500

    connection established with database

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,24 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Auth } from './auth/entities/auth.entity';
 
 
 @Module({
-  imports: [AuthModule, ConfigModule.forRoot({isGlobal:true})],
+  imports: [AuthModule, ConfigModule.forRoot({isGlobal:true}), 
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [Auth],
+      synchronize: true,
+    }),
+    TypeOrmModule.forFeature([Auth])
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
